f6d192cb6c79979d41ae6c76fb365cab0a6c0248
Gibransyah <gibransyah008@gmail.com>
2023-02-24 09:51:43 +0700

    updates 24-02-2023

--- rental_mobil.sql
+++ rental_mobil.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2023 at 02:17 PM
+-- Generation Time: Feb 24, 2023 at 03:50 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -42,7 +42,8 @@ INSERT INTO `merek` (`merek_id`, `nama`, `negara`) VALUES
 (1, 'Toyota', 'Jepang'),
 (2, 'Honda', 'Jepang'),
 (5, 'Suzuki', 'Jepang'),
-(6, 'Chevrolet', 'Amerika');
+(6, 'Chevrolet', 'Amerika'),
+(7, 'Wuling', 'Cina');
 
 -- --------------------------------------------------------
 
@@ -58,19 +59,21 @@ CREATE TABLE `mobil` (
   `kapasitas_mesin` int(11) NOT NULL,
   `harga_sewa` int(11) NOT NULL,
   `merek_id` int(11) NOT NULL,
-  `unit` int(11) NOT NULL DEFAULT 0
+  `status` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `mobil`
 --
 
-INSERT INTO `mobil` (`mobil_id`, `gambar`, `nama_mobil`, `jenis`, `kapasitas_mesin`, `harga_sewa`, `merek_id`, `unit`) VALUES
-(1, 'avanza.jpg', 'Avanza', 'Minibus', 1500, 100000, 1, 5),
-(2, 'Mobilio.jpg', 'Mobilio', 'Minibus', 1500, 120000, 2, 3),
-(5, 'Ertiga.jpg', 'Ertiga', 'Minibus', 1500, 100000, 5, 20),
-(7, '1675923843_73eced3a0af3bb747738.jpg', 'Jimny', 'Suv', 1300, 100000, 5, 50),
-(8, '1675936383_0be1493126063ce1dd99.jpg', 'Spark', 'City Car', 1000, 50000, 6, 20);
+INSERT INTO `mobil` (`mobil_id`, `gambar`, `nama_mobil`, `jenis`, `kapasitas_mesin`, `harga_sewa`, `merek_id`, `status`) VALUES
+(1, 'avanza.jpg', 'Avanza', 'Minibus', 1500, 100000, 1, 'disewa'),
+(2, 'Mobilio.jpg', 'Mobilio', 'Minibus', 1500, 120000, 2, 'disewa'),
+(5, 'Ertiga.jpg', 'Ertiga', 'Minibus', 1500, 100000, 5, 'disewa'),
+(7, '1675923843_73eced3a0af3bb747738.jpg', 'Jimny', 'Suv', 1300, 100000, 5, 'tersedia'),
+(8, '1675936383_0be1493126063ce1dd99.jpg', 'Spark', 'City Car', 1000, 50000, 6, 'tersedia'),
+(9, '1676253896_3e5ba6a77168fe4ee9a9.jpg', 'Almaz', 'Suv', 1500, 100000, 7, 'tersedia'),
+(10, '1676275846_37e653fc9b523d448ef1.jpg', 'Civic', 'CityCar', 1500, 100000, 2, 'tersedia');
 
 -- --------------------------------------------------------
 
@@ -91,8 +94,10 @@ CREATE TABLE `supir` (
 
 INSERT INTO `supir` (`supir_id`, `nama_supir`, `alamat`, `no_telp`) VALUES
 (1, 'Sugianto Permadi', 'Kp. Babakan Hilir', '085278912821'),
-(2, 'Petrin Beunta', 'Gg. Kulged', '086296211234'),
-(3, 'Ucup Sikit', 'Jl. Separo', '085727817581');
+(2, 'Petrin Pereum', 'Gg. Kulged', '086296211234'),
+(3, 'Ucup Sikit', 'Jl. Separo', '085727817581'),
+(4, 'Ujang Naon', 'Jl. Cukamandi', '086729123851'),
+(5, 'Sugeng', 'Jl.Cipanday', '08575895012');
 
 -- --------------------------------------------------------
 
@@ -104,24 +109,29 @@ CREATE TABLE `transaksi` (
   `transaksi_id` int(11) NOT NULL,
   `mobil_id` int(11) NOT NULL,
   `supir_id` int(11) NOT NULL,
+  `penyewa` varchar(225) NOT NULL,
   `tgl_keluar` date DEFAULT NULL,
   `tgl_masuk` date DEFAULT NULL,
-  `jam_keluar` time DEFAULT NULL,
-  `jam_masuk` time DEFAULT NULL,
-  `unit_total` int(11) NOT NULL,
-  `total` int(11) NOT NULL DEFAULT 0
+  `islunas` int(11) NOT NULL,
+  `nominal` int(11) NOT NULL,
+  `total` int(11) NOT NULL DEFAULT 0,
+  `status_pinjam` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`transaksi_id`, `mobil_id`, `supir_id`, `tgl_keluar`, `tgl_masuk`, `jam_keluar`, `jam_masuk`, `unit_total`, `total`) VALUES
-(22, 1, 1, '2023-02-09', NULL, '11:13:53', NULL, 5, 0),
-(23, 5, 1, '2023-02-09', '2023-02-09', '11:14:05', '12:41:10', 3, 301000),
-(24, 2, 2, '2023-02-09', '2023-02-09', '11:15:11', '13:24:36', 5, 602000),
-(25, 1, 2, '2023-02-08', '2023-02-09', '10:33:11', '11:34:38', 1, 200000),
-(26, 2, 1, '2023-02-09', NULL, '13:25:04', NULL, 2, 0);
+INSERT INTO `transaksi` (`transaksi_id`, `mobil_id`, `supir_id`, `penyewa`, `tgl_keluar`, `tgl_masuk`, `islunas`, `nominal`, `total`, `status_pinjam`) VALUES
+(45, 1, 1, 'Nenden', '2023-02-16', '2023-02-25', 1, 0, 900000, 'dikembalikan'),
+(46, 2, 3, 'Juwita', '2023-02-16', '2023-02-25', 1, 0, 1080000, 'dikembalikan'),
+(47, 5, 1, 'Junai', '2023-02-16', '2023-02-25', 1, 0, 900000, 'dikembalikan'),
+(75, 1, 1, 'Sigit', '2023-02-18', '2023-03-01', 1, 0, 500000, 'dikembalikan'),
+(104, 1, 1, 'Hani', '2023-02-20', '2023-02-21', 1, 0, 200000, 'dikembalikan'),
+(107, 2, 1, 'Utami', '2023-02-21', '2023-02-22', 1, 0, 240000, 'dipinjam'),
+(112, 5, 1, 'Hani', '2023-02-21', '2023-02-22', 1, 0, 200000, 'dipinjam'),
+(113, 1, 1, 'Rani', '2023-02-21', '2023-02-22', 1, 0, 200000, 'dipinjam'),
+(114, 7, 1, 'Lina', '2023-02-21', '2023-02-22', 3, 0, 0, 'dibatalkan');
 
 -- --------------------------------------------------------
 
@@ -142,7 +152,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`users_id`, `username`, `password`) VALUES
 (1, 'silwa', '$2y$10$RTJwxLSHu1JTf2d2K65qAOzg5LY2baIs3A1IxKXo7iOHP8Fa59.P.'),
 (2, 'gibran', '$2y$10$TkQshDSuWGOLiwvIM5xBpuC4wcHpPb9aSEa8zXtLQ4kzZQu569fce'),
-(3, 'rani', '$2y$10$BIDAoEPgd07Cc47WbnWeTuFLRGk.ucgp9qb56HvEjDAeUgBnTYXx6');
+(7, 'gibransyah', '$2y$10$PXCrMXAdz1WyNKmoe6HoturTRR0hc1K0wB/gm5dzKwrKbzMNVT296'),
+(8, 'kevin', '$2y$10$AcMbfRKP48MZLzmnkIvTTebIkVb7oxeS4ZWn/WOwh2LhhUceWbu.y');
 
 --
 -- Indexes for dumped tables
@@ -189,31 +200,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `merek`
 --
 ALTER TABLE `merek`
-  MODIFY `merek_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `merek_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `mobil`
 --
 ALTER TABLE `mobil`
-  MODIFY `mobil_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `mobil_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `supir`
 --
 ALTER TABLE `supir`
-  MODIFY `supir_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `supir_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `transaksi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `transaksi_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=115;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
